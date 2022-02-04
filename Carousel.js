c813413d8317ce91365e09772c83f39d0d31d3a1
Palak <98076904+PalakIndia@users.noreply.github.com>
2022-02-04 20:06:40 +0530

    Update Carousel.js

--- Carousel.js
+++ Carousel.js
@@ -1,37 +1,31 @@
-let prev =  document.querySelector('.prev');
+let prev = document.querySelector('.prev');
 let next = document.querySelector('.next');
+
 let rev = 0;
-carousel(rev);
+        carousel(rev);
+  
+       prev.addEventListener('click',()=>{
+           rev = rev-1;
+           carousel(rev);
+       })
 
-prev.addEventListener('click',()=>{
-    rev = rev - 1;
-    carousel(rev);
-})
-
-next.addEventListener('click',()=>{
-    rev = rev + 1;
-    carousel(rev);
-})
-
-
-function carousel(container) {
-    let reviews = document
-        .getElementsByClassName("profile-container");
-
-    if (container >= reviews.length) {
-        container = 0;
-        rev = 0;
-    }
-    if (container < 0) {
-        container = reviews.length - 1;
-        rev = reviews.length - 1;
-    }
-    for (let i = 0; i < reviews.length; i++) {
-        reviews[i].style.display = "none";
-    }
-    reviews[container].style.display = "block";
-}
-
-
-
-
+       next.addEventListener('click',()=>{
+        rev = rev+1;
+        carousel(rev);
+    })
+        function carousel(wrapper) {
+            let reviews = document.getElementsByClassName("profile-container");
+  
+            if (wrapper >= reviews.length) {
+                wrapper = 0;
+                rev = 0;
+            }
+            if (wrapper < 0) {
+                wrapper = reviews.length - 1;
+                rev = reviews.length - 1;
+            }
+            for (let i = 0; i < reviews.length; i++) {
+                reviews[i].style.display = "none";
+            }
+            reviews[wrapper].style.display = "block";
+        }
